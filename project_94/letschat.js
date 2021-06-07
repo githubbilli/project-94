eb8b5d12cabbb4682d6a2aa7f2da9d2796d9f1b5
githubbilli <78857712+githubbilli@users.noreply.github.com>
2021-06-07 18:09:31 +0600

    Add files via upload

---
+++ project_94/letschat.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC6raFLUo15lzZFy7pSODWHr0yrhoRBBr8",
+    authDomain: "lets-chat-kwitter.firebaseapp.com",
+    databaseURL: "https://lets-chat-kwitter-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-kwitter",
+    storageBucket: "lets-chat-kwitter.appspot.com",
+    messagingSenderId: "206586611239",
+    appId: "1:206586611239:web:1751188fa477db59bff002"
+  };
+
